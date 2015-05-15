3c4bd1cc075cc536aee5000f622a9803a38d874a
Danny Yao <dannyyaou@gmail.com>
2015-05-15 16:42:30 +0800

    done1

--- js/app.js
+++ js/app.js
@@ -1,36 +1,44 @@
-var EZTV = angular.module('EZTV', []);
+var EZTV = angular.module('EZTV', ['angular-loading-bar']);
 
-EZTV.controller('first_controller', function ($scope){
-	$scope.getShowList = function(){
-    var showList = {};
-    $.ajax({
-      async: false,
-      url:'http://localhost:8080/allshowlist',
-      type: 'GET',
-      dataType: 'json',
-      success: function(data) {
-        showsList = data;
-      }
-    });
-    return showsList;
-  };
+EZTV.controller('first_controller', function ($scope,$http){
+  $scope.showsList;
   $scope.getEpisodeList = function(href){
     var episodeList = {};
-    $.ajax({
-      async: false,
-      url:'http://localhost:8080/showlist?url='+href,
-      type: 'GET',
-      dataType: 'json',
-      success: function(data) {
-        episodeList = data;
-      }
+    $http.get('https://quiet-castle-3852.herokuapp.com/showlist?url='+href)
+    .success(function(response) {
+      console.log(response);
+      $scope.showEpisodes=response;
+      episodeList = response;
     });
     return episodeList;
   };
-  $scope.showsList = $scope.getShowList();
+
   $scope.init = function(){
-  	console.log($scope.showsList);
+    if(window.localStorage.likes == undefined){
+      window.localStorage.likes = "[]";
+    }
+    $scope.likes = JSON.parse(window.localStorage.likes);
+    console.log($scope.likes);
     $scope.video = false;
+    $http.get('https://quiet-castle-3852.herokuapp.com/allshowlist')
+    .success(function(response) {
+      var likenumber = 0;
+      $scope.showsList=response;
+      console.log($scope.showsList);
+      for (var i = $scope.showsList.length - 1; i >= 0; i--) {
+        for (var p = $scope.showsList[i].showslist.length - 1; p >= 0; p--) {
+          for (var j = $scope.likes.length - 1; j >= 0; j--) {
+            if ( $scope.showsList[i].showslist[p].showid == $scope.likes[j] + ".html" ){
+              likenumber ++ ;
+              $scope.showsList[i].showslist[p].like = 'liked';
+            }
+          }
+        }
+      }
+      console.log('you got '+ likenumber + 'likes');
+    });
+
+
   };
   $scope.showEpisode = function(){
   	this.next().toggle();
@@ -38,7 +46,7 @@ EZTV.controller('first_controller', function ($scope){
   $scope.showVideo = function(url){
     $scope.video = true;
     $('.playerSide_inside')
-      .append("<iframe class='videoEmbed' src='http://localhost:8080/videodiv?url="+url+"'></iframe>");
+      .append("<iframe class='videoEmbed' src='https://quiet-castle-3852.herokuapp.com/videodiv?url="+url+"'></iframe>");
   };
   $scope.videoClose = function(){
     $('.playerSide_inside').empty();
@@ -46,6 +54,7 @@ EZTV.controller('first_controller', function ($scope){
   };
   $scope.showEpisodes = [];
   $scope.showEpisodeList = function(href){
+    $scope.episodeListShow = true;
     console.log(href);
     $scope.showEpisodes = [];
     $scope.showEpisodes = $scope.getEpisodeList(href);
@@ -53,6 +62,47 @@ EZTV.controller('first_controller', function ($scope){
   };
   $scope.changeSearch = function(stuff){
     $scope.search = stuff;
+  };
+  $scope.episodeListShow = false;
+  $scope.closeBtn1 = function(){
+    $scope.episodeListShow = false;
+  };
+  $scope.random = function() {
+        return 0.5 - Math.random();
+  };
+  $scope.like = function(url){
+    var iai = false;
+    var indexPlusOne;
+    for (var i = 0; i < $scope.likes.length; i++) {
+      if ( $scope.likes[i] == url ){
+        iai = true;
+        indexPlusOne = i;
+      }
+    }
+    if(!iai){
+      $scope.likes.push(url);
+      window.localStorage.likes = JSON.stringify($scope.likes);
+      $('img.'+url).attr('src','images/liked.png');
+      for (var i = $scope.showsList.length - 1; i >= 0; i--) {
+        for (var p = $scope.showsList[i].showslist.length - 1; p >= 0; p--) {
+          if ( $scope.showsList[i].showslist[p].showid == $scope.likes[$scope.likes.length-1] + ".html" ){
+            $scope.showsList[i].showslist[p].like = 'liked';
+          }
+        }
+      }
+    }
+    if (iai) {
+      $('img.'+url).attr('src','images/booshihuan.png');
+      for (var i = $scope.showsList.length - 1; i >= 0; i--) {
+        for (var p = $scope.showsList[i].showslist.length - 1; p >= 0; p--) {
+          if ( $scope.showsList[i].showslist[p].showid == $scope.likes[indexPlusOne] + ".html" ){
+            $scope.showsList[i].showslist[p].like = 'booshihuan';
+          }
+        }
+      }
+      $scope.likes.splice(indexPlusOne, 1);
+      window.localStorage.likes = JSON.stringify($scope.likes);
+    };
   };
 });
 
